3d431499a971243b7c69eff370f617d86bcd7bbc
Shelly Tang <shellytang@gmail.com>
2017-07-10 15:19:40 -0700

    day2 setup

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,24 +1,30 @@
-const ce = React.createElement;
-
-const myTitle = function(props) {
+import React from 'react';  // similar to using require;
+import { render } from 'react-dom';
+// { } only include pieces specified betwen the braces
+const MyTitle = function(props) {
   // parameters -> parent element, attributes, child element
-  return ce(
-    "div",
-    null,
-    ce("h1", { style: { color: props.color } }, props.title)
+  // return ce(
+  //   "div",
+  //   null,
+  //   ce("h1", { style: { color: props.color } }, props.title)
+  // );
+  // above is pre-babel;
+  return (
+    <div>
+      <h1 style={ {color: props.color} }>{props.title}</h1>
+    </div>
   );
 };
 const MyFirstComponent = function() {
   // example of nested component
-  //multiple instances of myTitle
-  return ce(
-    "div",
-    { id: "my-first-component" },
-    ce(myTitle, { title: "Game of Thrones", color: "YellowGreen" }),
-    ce(myTitle, { title: "Stranger Things", color: "GreenYellow" }),
-    ce(myTitle, { title: "Rick and Morty", color: "LimeGreen" }),
-    ce(myTitle, { title: "Silicon Valley", color: "Lime" })
-  );
+  // multiple instances of myTitle
+  // self-closing tags is part of jsx
+  return <div id="my-first-component">
+    <MyTitle title="Game of Thrones" color="YellowGreen" />
+    <MyTitle title="Stranger Things" color="GreenYellow" />
+    <MyTitle title="Rick and Morty" color="LimeGreen" />
+    <MyTitle title="Silicon Valley" color="Lime" />
+  </div>
 };
 
-ReactDOM.render(ce(MyFirstComponent), document.getElementById("app"));
+render(<MyFirstComponent />, document.getElementById("app"));
