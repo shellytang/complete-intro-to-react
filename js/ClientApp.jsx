a659da3abd7c41d1ba0b4c2a7a7a694dbe0a5f8b
Shelly Tang <shellytang@gmail.com>
2017-07-10 23:58:53 -0700

    created landing and import router

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,30 +1,15 @@
-import React from 'react';  // similar to using require;
+import React from 'react';
 import { render } from 'react-dom';
-// { } only include pieces specified betwen the braces
-const MyTitle = function(props) {
-  // parameters -> parent element, attributes, child element
-  // return ce(
-  //   "div",
-  //   null,
-  //   ce("h1", { style: { color: props.color } }, props.title)
-  // );
-  // above is pre-babel;
-  return (
-    <div>
-      <h1 style={ {color: props.color} }>{props.title}</h1>
-    </div>
-  );
-};
-const MyFirstComponent = function() {
-  // example of nested component
-  // multiple instances of myTitle
-  // self-closing tags is part of jsx
-  return <div id="my-first-component">
-    <MyTitle title="Game of Thrones" color="YellowGreen" />
-    <MyTitle title="Stranger Things" color="GreenYellow" />
-    <MyTitle title="Rick and Morty" color="LimeGreen" />
-    <MyTitle title="Silicon Valley" color="Lime" />
-  </div>
-};
+import { HashRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
 
-render(<MyFirstComponent />, document.getElementById("app"));
+const App = () => (
+  <HashRouter>
+    <div className="app">
+      <Route exact path='/' component={Landing} />
+    </div>
+  </HashRouter>
+
+);
+
+render(<App />, document.getElementById('app'));
