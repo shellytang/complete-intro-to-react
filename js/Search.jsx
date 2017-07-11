457a3e641d91a59687065bc5ed2e7b7bce6b25c5
Shelly Tang <shellytang@gmail.com>
2017-07-11 14:38:22 -0700

    rendered list of shows with details

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import React from "react";
+import preload from "../data.json";
 
-const Search = () => <h1>hi lol this is search</h1>
+const Search = () => (
+  <div className="search">
+    {preload.shows.map(show => (
+      <div className="show-card">
+        <img
+          alt={`${show.title} Show Poster`}
+          src={`/public/img/posters/${show.poster}`}
+        />
+        <div>
+          <h3>{show.title}</h3>
+          <h4>{show.year}</h4>
+          <p>{show.description}</p>
+        </div>
+      </div>
+    ))}
+  </div>
+);
 
 export default Search;
